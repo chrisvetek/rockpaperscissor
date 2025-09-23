a1c92aef9f51c3dad49ab4a56cdae27aec228120
chrisvetek <chrisvetek@ymail.com>
2025-09-22 18:47:09 -0700

    started coding game

--- rps.js
+++ rps.js
@@ -1,3 +1,19 @@
+console.log("hello world");
 function getComputerChoice(){
+
+    let x = "rock";
+    let y = "paper";
+    let z = "scissors";
+
+    result1 = Math.random(0,1);
+    console.log(x + " gets " + result1);
+    result2 = Math.random(0,1);
+    console.log(y + " gets " + result2);
+    result3 = Math.random(0,1);
+    console.log(z + " gets " + result3);
+
+    console.log("Paper beats rock, rock beats scissors, scissors beats paper");
     
 }
+
+console.log(getComputerChoice());
